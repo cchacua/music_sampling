6bffcb707df302980f349efbcc842f492a7d6b08
Christian Chacua <cchacua@gmail.com>
2018-08-06 16:17:52 +0200

    06082018 1617

---
+++ scripts/procedures/descriptive/demo.sql
@@ -0,0 +1,14 @@
+--------------------------------------------------------------------------
+-- PRELIMINARY QUERYS
+--------------------------------------------------------------------------
+
+
+-- List of databases
+\t
+
+-- List of tables
+\dt
+
+-- Select tables 
+
+SELECT a.* FROM musicbrainz.recording a LIMIT 10 OFFSET 2;
